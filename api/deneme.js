736dcbf79916b97340d5d8148ae27a942ceaa779
halukhndstn <145353400+halukhndstn@users.noreply.github.com>
2025-01-17 04:38:24 +0300

    Delete deneme.js

--- api/deneme.js
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcrypt';
-
-const hash = '$2b$10$08OceFGf6K/K9iAKhmX5f.zwBSa1rDY4jIfslJu2MKNZhoRyTYvc2'; // Veritabanındaki hash
-const plainPassword = 'kullaniciGirisiSifresi'; // Giriş sırasında gelen şifre
-
-bcrypt.compare(plainPassword, hash).then((isMatch) => {
-  console.log('Password match:', isMatch);
-});
